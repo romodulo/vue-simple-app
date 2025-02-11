10f13217bcab0615607a112cfb0f1be7c487829c
romel-romodulo <57033860+romodulo@users.noreply.github.com>
2025-02-11 23:58:45 +1000

    First commit < 2-11>  <11:58>: created a new-vue-app

---
+++ vue-frontend/src/MyApp.vue
@@ -0,0 +1,10 @@
+<script setup>
+import { ref } from 'vue'
+
+const variableOne = ref("test-variable")
+
+</script>
+
+<template>
+    <h1>{{ variableOne }}</h1>
+</template>
---
+++ vue-frontend/src/main.js
@@ -0,0 +1,7 @@
+import './assets/main.css'
+
+import { createApp } from 'vue'
+import App from './App.vue'
+import MyApp from './MyApp.vue'
+
+createApp(MyApp).mount('#app')
