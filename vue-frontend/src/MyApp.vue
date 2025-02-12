4e53741cbe133f208ee980217e533861b13f8b1c
romel-romodulo <57033860+romodulo@users.noreply.github.com>
2025-02-12 22:41:18 +1000

    First commit of < 2-12>  <22:28>: added files::  soon to delete files: unneccessary-vue-components

--- vue-frontend/src/MyApp.vue
+++ vue-frontend/src/MyApp.vue
@@ -1,10 +1,31 @@
 <script setup>
 import { ref } from 'vue'
 
-const variableOne = ref("test-variable")
+const variableOne = ref("text-input")
+
+const functionDefinition = () => {return variableOne.value = ""}
 
 </script>
 
 <template>
-    <h1>{{ variableOne }}</h1>
+    <div class="definedFlex">
+        <h1>{{ variableOne }}</h1>
+        <input class="definedInputElement"v-model="variableOne">
+        <button class="definedInputElement buttonElement" v-on:click="functionDefinition">Submit</button>
+    </div>
 </template>
+
+<style scoped>
+.definedFlex {
+    display: flex;
+    flex-direction: column;
+}
+.definedInputElement {
+    /* display: block; */
+    height: 48px;
+    margin-top: 3px;
+}
+.buttonElement {
+    margin-top: 7px;
+}
+</style>
